2cef7ed343868f27e9b18526de927752219388d6
lollapaloooza <xusainovx@gmail.com>
2022-01-03 17:32:45 +0500

    added header component and links to home, shop, contact, sign in pages

--- src/App.js
+++ src/App.js
@@ -3,13 +3,17 @@ import ShopPage from "./pages/shop/shop.component";
 
 import {Route, Switch} from 'react-router-dom';
 import './App.css';
+import Header from "./components/header/header.component";
 
 function App() {
   return (
-    <Switch>
-        <Route exact path='/' component={Homepage}/>
-        <Route path='/shop' component={ShopPage}/>
-    </Switch>
+      <div>
+          <Header/>
+          <Switch>
+              <Route exact path='/' component={Homepage}/>
+              <Route path='/shop' component={ShopPage}/>
+          </Switch>
+      </div>
   );
 }
 
